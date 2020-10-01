21fe24a72f1dc52b627077e4a5f4c1bed75713b2
Lhaíslla Cavalcanti <lhaislla@gmail.com>
2020-10-01 20:54:51 -0300

    Update BD_Startup.sql

--- BD_Startup.sql
+++ BD_Startup.sql
@@ -56,21 +56,21 @@ commit;
 
 -- Respostas da letra b da segunda questão da lista 05:
 -- I:
--- SELECT nome_programador,nome_startup FROM  Programador,Startup WHERE Programador.id_startup = Startup.id_startup;
--- SELECT nome_programador,nome_startup FROM  Programador INNER JOIN Startup ON Programador.id_startup = Startup.id_startup;
--- SELECT nome_programador,nome_startup FROM Programador NATURAL JOIN  Startup;
+SELECT nome_programador,nome_startup FROM  Programador,Startup WHERE Programador.id_startup = Startup.id_startup;
+SELECT nome_programador,nome_startup FROM  Programador INNER JOIN Startup ON Programador.id_startup = Startup.id_startup;
+SELECT nome_programador,nome_startup FROM Programador NATURAL JOIN  Startup;
 -- II:
--- SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  JOIN Programador AS P ON PL.id_programador = P.id_programador;
--- III: SELECT DISTINCT TAB.* FROM ( SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  LEFT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  LEFT JOIN Programador AS P ON PL.id_programador = P.id_programador UNION ALL SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  RIGHT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  RIGHT JOIN Programador AS P ON PL.id_programador = P.id_programador) AS TAB;
--- SELECT DISTINCT TAB.* FROM ( SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  LEFT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  LEFT JOIN Programador AS P ON PL.id_programador = P.id_programador UNION ALL SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  RIGHT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  RIGHT JOIN Programador AS P ON PL.id_programador = P.id_programador) AS TAB;
+SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  JOIN Programador AS P ON PL.id_programador = P.id_programador;
+-- III:SELECT DISTINCT TAB.* FROM ( SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  LEFT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  LEFT JOIN Programador AS P ON PL.id_programador = P.id_programador UNION ALL SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  RIGHT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  RIGHT JOIN Programador AS P ON PL.id_programador = P.id_programador) AS TAB;
+SELECT DISTINCT TAB.* FROM ( SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  LEFT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  LEFT JOIN Programador AS P ON PL.id_programador = P.id_programador UNION ALL SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  RIGHT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  RIGHT JOIN Programador AS P ON PL.id_programador = P.id_programador) AS TAB;
 -- IV: 
--- SELECT DISTINCT P.nome_programador FROM Programador AS P WHERE P.id_programador NOT IN (SELECT id_programador FROM Programador_Linguagem);
+ SELECT DISTINCT P.nome_programador FROM Programador AS P WHERE P.id_programador NOT IN (SELECT id_programador FROM Programador_Linguagem);
 -- V:
--- SELECT DISTINCT TAB.* FROM (SELECT S.nome_startup, P.nome_programador FROM Startup AS S LEFT JOIN Programador AS P ON P.id_startup = S.id_startup UNION ALL SELECT S.nome_startup, P.nome_programador FROM Startup AS S RIGHT JOIN Programador AS P ON P.id_startup = S.id_startup) AS TAB;
+SELECT DISTINCT TAB.* FROM (SELECT S.nome_startup, P.nome_programador FROM Startup AS S LEFT JOIN Programador AS P ON P.id_startup = S.id_startup UNION ALL SELECT S.nome_startup, P.nome_programador FROM Startup AS S RIGHT JOIN Programador AS P ON P.id_startup = S.id_startup) AS TAB;
 -- VI:
--- SELECT S.nome_startup FROM Startup AS S WHERE S.nome_startup NOT IN(SELECT S.nome_startup FROM Startup AS S JOIN Programador AS P ON P.id_startup = S.id_startup);
+SELECT S.nome_startup FROM Startup AS S WHERE S.nome_startup NOT IN(SELECT S.nome_startup FROM Startup AS S JOIN Programador AS P ON P.id_startup = S.id_startup);
 -- Letra C:
--- SELECT DISTINCT TAB.* FROM ( SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  LEFT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  LEFT JOIN Programador AS P ON PL.id_programador = P.id_programador UNION ALL SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  RIGHT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  RIGHT JOIN Programador AS P ON PL.id_programador = P.id_programador) AS TAB; 
+SELECT DISTINCT TAB.* FROM ( SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  LEFT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  LEFT JOIN Programador AS P ON PL.id_programador = P.id_programador UNION ALL SELECT P.nome_programador, LP.linguagem FROM Programador_Linguagem AS PL  RIGHT JOIN Linguagem_Programação AS LP ON PL.id_linguagem  = LP.id_linguagem  RIGHT JOIN Programador AS P ON PL.id_programador = P.id_programador) AS TAB; 
 
 
 
